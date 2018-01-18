0ac04e1c13ec5cd7a6e1428ade8a25c43d7d944b
Danny Straus <strausd@me.com>
2018-01-17 20:54:39 -0600

    Setup header background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,17 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className="header">
-            <h1>Danny Straus</h1>
-            <ul className="nav">
-                <li className="nav-button"><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
-                <li className="nav-button"><NavLink to="/resume" activeClassName="active">Resume</NavLink></li>
-                <li className="nav-button"><NavLink to="/projects" activeClassName="active">Projects</NavLink></li>
-                <li className="nav-button"><NavLink to="/about" activeClassName="active">About</NavLink></li>
-            </ul>
+        <div className="header__container">
+            <div className="header__content">
+                <h1 className="header__title">Danny Straus</h1>
+                <ul className="header__nav-list">
+                    <li className="header__nav-button"><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
+                    <li className="header__nav-button"><NavLink to="/resume" activeClassName="active">Resume</NavLink></li>
+                    <li className="header__nav-button"><NavLink to="/projects" activeClassName="active">Projects</NavLink></li>
+                    <li className="header__nav-button"><NavLink to="/about" activeClassName="active">About</NavLink></li>
+                </ul>
+            </div>
+            <div className="header__transition"></div>
         </div>
     );
 };

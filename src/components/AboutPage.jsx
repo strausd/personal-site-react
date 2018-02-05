97dce1f18668e3ca7fa40498693482dd4b6f9fa5
Danny Straus <strausd@me.com>
2018-02-05 08:07:28 -0600

    Update about page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -7,9 +7,9 @@ const AboutPage = () => {
             <div className="resume-section">
                 <h2 className="resume-header">Hi, I'm Danny.</h2>
                 <div className="resume-section-body">
-                    <p>My background is in 3D animation and visual effects. I quickly gravitated towards the technical aspects of 3D. Eventually I became my team's developer, creating automation tools in Python to enhance and speed up artist's workflow.</p>
+                    <p>I am currently a software engineer at an awesome company called Insite Interactive. We build custom software, websites, and mobile applications for anybody and everybody. My job as a developer is to build responsive front end sites using HTML, CSS, and JavaScript along with robust backends for large scale projects using Python, Django, and PostgreSQL. Working at a small company like Insite allows me to own a project, which involves working directly with the client to gather requirements, negotiate timelines, and make decisions regarding the technical architecture of the project.</p>
+                    <p>Prior to Insite, I worked in 3D animation and visual effects. I quickly gravitated towards the technical aspects of 3D. Eventually I became my team's developer, creating automation tools in Python to enhance and speed up artist's workflow.</p>
                     <p>My passion for software development grew with each new tool I built. Before long, I realized I didn't just want software development to be part of my job, but my entire job.</p>
-                    <p>I am currently a software engineer at an awesome company called Insite Interactive. We build custom software, websites, and mobile applications for anybody and everybody. My job as a developer is to build robust backends for large scale projects using Python, Django, and PostgreSQL. I also build responsive front end sites using HTML, CSS, and JavaScript. Working at a small company like Insite allows me to own a project, which involves working directly with the client to gather requirements, negotiate timelines, and make decisions regarding the technical architecutre of the project.</p>
                 </div>
             </div>
             <div className="resume-section">
